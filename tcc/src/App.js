1d83301c1ed612947dcebebc417dc26cc8c60713
souzateuz <matheusssouza1112@gmail.com>
2025-04-01 15:35:25 -0300

    app

---
+++ tcc/src/App.js
@@ -0,0 +1,14 @@
+import './index.css'
+import './App.css';
+import sla from './imagens/download.jpg';
+import teste from './imagens/aizen.jpg';
+import luva from './imagens/luva.jpg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  
+}
+
+export default App;
