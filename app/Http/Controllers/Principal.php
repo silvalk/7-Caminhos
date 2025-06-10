76bc0cf33c14706bb5cc4ff3b34bbd197d306dae
silvalk <lucas.silva65729@gmail.com>
2025-06-10 15:18:14 -0300

    Laravel instalado

---
+++ app/Http/Controllers/Principal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Principal extends Controller
+{
+    function principal(){
+        return View('principal');
+    }
+}
+
